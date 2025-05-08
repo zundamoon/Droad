03196c26ba9c84ff13bc64e8d018eead0071c7c1
Ishihara <mcd2376050@stu.o-hara.ac.jp>
2025-05-08 12:44:07 +0900

    UIのサンプルを制作

---
+++ Assets/Ishihara/Script/SystemObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Ishihara
+ * マネージャークラスの基底
+ */
+public class SystemObject 
+{
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    public void Initialize()
+    {
+        // 初期化処理
+    }
+    public void
+}
