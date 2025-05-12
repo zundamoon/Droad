f725670c2ec0629b4101afaf19883b726c47387d
Ishihara <mcd2376050@stu.o-hara.ac.jp>
2025-05-12 09:55:35 +0900

    コモンモジュールを更新

--- Assets/Scripts/Common/CommonModule.cs
+++ Assets/Scripts/Common/CommonModule.cs
@@ -22,6 +22,58 @@ public class CommonModule
     }
 
     /// <summary>
+	/// リストの初期化
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="list"></param>
+	/// <param name="capacity"></param>
+	public static void InitializeList<T>(ref List<T> list, int capacity = -1)
+    {
+        if (list == null)
+        {
+            if (capacity < 0)
+            {
+                list = new List<T>();
+            }
+            else
+            {
+                list = new List<T>(capacity);
+            }
+        }
+        else
+        {
+            if (list.Capacity < capacity) list.Capacity = capacity;
+
+            list.Clear();
+        }
+    }
+
+    /// <summary>
+	/// 配列が空か否か
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="array"></param>
+	/// <returns></returns>
+	public static bool IsEmpty<T>(T[] array)
+    {
+        return array == null || array.Length == 0;
+    }
+
+    /// <summary>
+    /// 配列に対して有効なインデクスか否か
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="array"></param>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public static bool IsEnableIndex<T>(T[] array, int index)
+    {
+        if (IsEmpty(array)) return false;
+
+        return array.Length > index && index >= 0;
+    }
+
+    /// <summary>
     /// リストが空か否か
     /// </summary>
     /// <typeparam name="T"></typeparam>
@@ -30,6 +82,40 @@ public class CommonModule
     public static bool IsEmpty<T>(List<T> checkList)
     {
         return checkList == null || checkList.Count == 0;
+    }
+
+    /// <summary>
+	/// リストに対して有効なインデクスか否か
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="list"></param>
+	/// <param name="index"></param>
+	/// <returns></returns>
+	public static bool IsEnableIndex<T>(List<T> list, int index)
+    {
+        if (IsEmpty(list)) return false;
+
+        return list.Count > index && index >= 0;
+    }
+
+    /// <summary>
+    /// リストを重複なしでマージ
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="main"></param>
+    /// <param name="sub"></param>
+    public static void MeargeList<T>(ref List<T> main, List<T> sub)
+    {
+        if (IsEmpty(sub)) return;
+
+        if (main == null) main = new List<T>();
+
+        for (int i = 0, max = sub.Count; i < max; i++)
+        {
+            if (main.Exists(mainElem => mainElem.Equals(sub[i]))) continue;
+
+            main.Add(sub[i]);
+        }
     }
 
     /// <summary>
