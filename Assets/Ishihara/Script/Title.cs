92ea0693cfbc2007bdbe276ecd3f58ea80f4517f
Ishihara <mcd2376050@stu.o-hara.ac.jp>
2025-06-24 09:45:06 +0900

    やり直したときに参照エラーを解決

--- Assets/Ishihara/Script/Title.cs
+++ Assets/Ishihara/Script/Title.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Title : MonoBehaviour
 {
     [SerializeField] DontDestroy destroy;
+    [SerializeField] AudioManager audioManager;
 
     // Start is called before the first frame update
     void Start()
     {
         destroy.CheckInstance();
+        if (AudioManager.instance == null) AudioManager.instance = audioManager;
+        audioManager.Init();
         AudioManager.instance.PlayBGM(GameEnum.BGM.TITLE);
     }
 }
